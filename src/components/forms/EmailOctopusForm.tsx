3f36a2f72f8e17d4a8cb173a086ec91b58b3184c
James Villarrubia <james.villarrubia@gmail.com>
2025-02-14 10:46:20 -0500

    adds mininmum height for form

--- src/components/forms/EmailOctopusForm.tsx
+++ src/components/forms/EmailOctopusForm.tsx
@@ -1,13 +1,16 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export const EmailOctopusForm = () => {
   const myRef = useRef<HTMLDivElement>(null);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     const script = document.createElement('script');
     script.src = 'https://eocampaign1.com/form/0f4dcb86-eade-11ef-98ed-0d5253b99da6.js';
     script.async = true;
     script.setAttribute('data-form', "0f4dcb86-eade-11ef-98ed-0d5253b99da6");
+
+    script.onload = () => setIsLoaded(true);
 
     if(myRef.current){
       const myRefNode = myRef.current
@@ -22,7 +25,11 @@ export const EmailOctopusForm = () => {
 
   return (
     <>
-      <div ref={myRef}></div>
+      <div 
+        ref={myRef} 
+        className={`min-h-[307px] flex items-center justify-center ${!isLoaded ? 'bg-gray-50' : ''}`}
+      >
+      </div>
     </>
   );
 };
